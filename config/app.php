28cfb272945301efb0a24add6f249599ca3e8576
briefs33 <capsule.briefs@gmail.com>
2021-12-11 18:57:24 +0100

    pridanie providerov a aliasov

--- config/app.php
+++ config/app.php
@@ -161,6 +161,9 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Laracasts\Generators\GeneratorsServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,6 +232,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Form' => Collective\Html\HtmlFacade::class,
+
 
     ],
 

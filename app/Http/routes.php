9fc1c53b859a975d05313134c2e60e3f46440ad0
briefs33 <capsule.briefs@gmail.com>
2021-12-11 20:20:57 +0100

    skuska routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,14 @@
 <?php
-
-Routr::get('/',function(){
+/*
+Route::get('/',function(){
 	return view('welcome');
+});
+*/
+Route::get('/', function()
+{
+    return 'Hello World';
+});
+
+Route::get('blog',function(){
+	return 'this is a blog';
 });

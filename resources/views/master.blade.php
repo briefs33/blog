65ecdc1db4d7a2d605f1f3deb1e352aab6abdcbf
briefs33 <capsule.briefs@gmail.com>
2021-12-13 14:27:33 +0100

    upravenie titulku v master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('tittle') / blog</title>
+        <title>@yield('tittle')</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
 

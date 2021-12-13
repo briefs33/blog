2970b75e35fecef744c99636802e6304248953ab
briefs33 <capsule.briefs@gmail.com>
2021-12-13 08:38:33 +0100

    upravenie showx.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,18 +1,21 @@
 @extends('master')
-@section('tittle','názov postu')
+@section('tittle', $post->title)
 
 @section('content')
 	<section class="box">
 		<article class="post full-post">
 			<header class="post-header">
 				<h1 class="box-heading">
-					<a href="">názov postu</a>
+					<a href="{{ URL::current() }}">{{ $post->title }}</a>
+					<time datetime="">
+						<small>{{ $post->created_at }}</small>
+					</time>
 				</h1>
 			</header>
 
 			<div class="post-content">
 				<p>
-					tu budú posty
+					{{ $post->text }}
 				</p>
 			</div>
 		</article>

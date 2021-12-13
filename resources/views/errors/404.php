536dd548e31b43ab35bc5be25f096f8e57cd13c1
briefs33 <capsule.briefs@gmail.com>
2021-12-13 09:05:03 +0100

    upravenie zobrazenia 404

--- resources/views/errors/404.php
+++ resources/views/errors/404.php
@@ -1,14 +1,15 @@
-@extends('master')
+<!--@extends('master')
 @section('tittle', '404')
 
 @section('content')
+-->
     <section class="box">
-        <h1>404</h1>
+        <h1>Chyba 404</h1>
 
-        <h2>Stránka sa nenašla</h2>
+        <p>Stránka sa nenašla</p>
 
         <a href="/">
             <button>Späť</button>
         </a>
     </section>
-@endsection
+<!--@endsection-->

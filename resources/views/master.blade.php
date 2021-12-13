091c1adcf2d3c8edebadc4238f9912af0baab72c
briefs33 <capsule.briefs@gmail.com>
2021-12-13 15:06:39 +0100

    upravenie header v master.blade.php

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,7 +10,27 @@
 
     <body>
         <header class="container">
+            @if (count($errors) > 0)
+                <ul class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            @endif
 
+            @if (Auth::check())
+                <nav class="navigation">
+                    <div class="btn-group btn-group-sm pull-left">
+                        <a href="{{ url('/') }}" class="btn btn-default">all posts</a>
+                        <a href="{{ url('user/' . Auth::id()) }}" class="btn btn-default">my posts</a>
+                        <a href="{{ url('post/create') }}" class="btn btn-default">add new</a>
+                    </div>
+                    <div class="btn-group btn-group-sm pull-right">
+                        <span class="username small">{{ Auth::user()->email }}</span>
+                        <a href="{{ url('auth/logout') }}" class="btn btn-default logout">logout</a>
+                    </div>
+                </nav>
+            @endif
         </header>
 
         <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">

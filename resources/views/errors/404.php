67565b9c35ad6f2c4b0b63c26e4059fcc535a154
briefs33 <capsule.briefs@gmail.com>
2021-12-13 09:00:28 +0100

    vytvorenie wiews/errors pre 404

---
+++ resources/views/errors/404.php
@@ -0,0 +1,14 @@
+@extends('master')
+@section('tittle', '404')
+
+@section('content')
+    <section class="box">
+        <h1>404</h1>
+
+        <h2>Stránka sa nenašla</h2>
+
+        <a href="/">
+            <button>Späť</button>
+        </a>
+    </section>
+@endsection

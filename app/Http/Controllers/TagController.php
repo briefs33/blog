0a1e7a698d0e405b83e5c64d2ecfd19bb34dbea6
briefs33 <capsule.briefs@gmail.com>
2021-12-13 20:28:46 +0100

    doplnenie funkcie show do TagController_a

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+//use App\User;
 use Illuminate\Http\Request;
+//use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class TagController extends Controller
 {
     public function show($id){
+        $tag=\App\Models\Tag::findOrFail($id);
 
+        return view('posts.index')
+        ->with('title',$tag->name)
+        ->with('posts',$tag->posts);
     }
 }

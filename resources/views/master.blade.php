387dd72622635f12efc346536322230a1c9c08bc
briefs33 <capsule.briefs@gmail.com>
2021-12-13 15:33:49 +0100

    zlúčenie head do nav

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,30 +9,6 @@
     </head>
 
     <body>
-        <header class="container">
-            @if (count($errors) > 0)
-                <ul class="alert alert-danger">
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            @endif
-
-            @if (Auth::check())
-                <nav class="navigation">
-                    <div class="btn-group btn-group-sm pull-left">
-                        <a href="{{ url('/') }}" class="btn btn-default">all posts</a>
-                        <a href="{{ url('user/' . Auth::id()) }}" class="btn btn-default">my posts</a>
-                        <a href="{{ url('post/create') }}" class="btn btn-default">add new</a>
-                    </div>
-                    <div class="btn-group btn-group-sm pull-right">
-                        <span class="username small">{{ Auth::user()->email }}</span>
-                        <a href="{{ url('auth/logout') }}" class="btn btn-default logout">logout</a>
-                    </div>
-                </nav>
-            @endif
-        </header>
-
         <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
             <div class="container">
                 <a class="navbar-brand mr-auto" href="#">PositronX</a>
@@ -41,18 +17,38 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
+                    @if (count($errors) > 0)
+                        <ul class="alert alert-danger">
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+
                     <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/') }}">all posts</a>
+                        </li>
                         @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">Login</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register-user') }}">Register</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register-user') }}">Register</a>
+                            </li>
                         @else
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('signout') }}">Logout</a>
-                        </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('user/' . Auth::id()) }}">my posts</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ url('post/create') }}">add new</a>
+                            </li>
+                            <li class="nav-item">
+                                {{ Auth::user()->email }}
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('signout') }}">Logout</a>
+                            </li>
                         @endguest
                     </ul>
                 </div>

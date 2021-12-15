cbcc746499bbaf7ad8b530ea0612e035fd0d8ed6
briefs33 <capsule.briefs@gmail.com>
2021-12-15 03:49:05 +0100

    opravenie chyby -> php artisan up

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+@section('tittle', '503')
+
+@section('content')
+    <section class="box">
+        <h1>Chyba 503</h1>
+
+        <p>Unavailable Service</p>
+
+        <a href="/">
+            <button>Domov</button>
+        </a>
+
+    </section>
+@endsection
